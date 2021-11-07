b79acdf7bd03f8916f16e662cfc1db6073187609
NURGUL <nurgulfirat02@gmail.com>
2021-11-07 23:53:38 +0300

    .

--- src/weekTwoHomework.js
+++ src/weekTwoHomework.js
@@ -14,10 +14,9 @@ function findPrime(...numbers) {
         }
     }    
 }
-
 findPrime(5,13,24,2,28);
 
-//Parametre olarak girilen iki sayının arkadaş sayılar olup olmadığını bulan programı yazınız.
+//2- Parametre olarak girilen iki sayının arkadaş sayılar olup olmadığını bulan programı yazınız.
 function friendNumber(number1, number2){
     let sum1 = 0
     let sum2 = 0
@@ -38,6 +37,44 @@ function friendNumber(number1, number2){
         console.log(number1 + " and " + number2 + " is not friend number.")
     }
 }
-
 friendNumber(220,284) //is friend number
 friendNumber(100, 250) //is not friend number
+
+//3- 1000'e kadarki tüm mükemmel sayıları listeleyen programı yazınız.
+function perfectNumber(){
+    let sum
+    for (let i = 2; i < 1000; i++) {
+        sum = 0
+        for (let j = 1; j <= (i/2); j++) {
+            if (i % j == 0) {
+                sum = sum + j
+            }            
+        }
+        if (i == sum) {
+            console.log(i)
+        }        
+    }
+}
+perfectNumber()
+
+//4- 1000'e kadarki tüm asal sayıları listeleyen programı yazınız.
+function findPrime2(){
+ 
+    for (let number = 2; number <= 1000; number++) {
+        let control = 0; 
+        for (let i = 2; i < number; i++) {
+            if (number % i == 0) 
+            {
+                control = 1;
+                break;
+            }           
+        }
+        if (control == 1) {
+            //console.log(i + " asal değildir.")
+        }
+        else{
+            console.log(i + " is prime.")
+        }        
+    }
+}
+findPrime2()
